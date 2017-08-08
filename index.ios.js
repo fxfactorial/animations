207309504912481b4e4e1728d072dfb71a63f795
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-08-08 01:43:16 -0700

    without tern project

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import animations from './lib/animation-1';
+
+AppRegistry.registerComponent('animations', () => animations);
